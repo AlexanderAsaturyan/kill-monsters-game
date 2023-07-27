d14b770621888bf131d3f08b5561f21e265326bb
AlexanderAsaturyan <alexanderasaturyan@gmail.com>
2023-07-26 21:13:34 -0600

    Fixed scoreText and FreezeSpawn ui bugs.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 using TMPro;
 

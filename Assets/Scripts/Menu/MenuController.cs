e14072391babd0892dac53790480c40bb2e6753a
AlexanderAsaturyan <alexanderasaturyan@gmail.com>
2023-07-30 02:25:40 +0400

    Finished credits view.

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -5,8 +5,13 @@ using UnityEngine.UI;
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private Button newGameButton;
-    [SerializeField] private Button recordButton;
+    [SerializeField] private RectTransform mainMenu;
+    [SerializeField] private RectTransform recordsView;
+    [SerializeField] private RectTransform creditsView;
+    [SerializeField] private Button recordsButton;
+    [SerializeField] private Button recordsBackButton;
     [SerializeField] private Button creditsButton;
+    [SerializeField] private Button creditsBackButton;
     [SerializeField] private Button quitGame;
     [SerializeField] private AudioSource menuMusic;
 
@@ -14,12 +19,40 @@ public class MenuController : MonoBehaviour
     {
         menuMusic.Play();
         newGameButton.onClick.AddListener(OpenNewGame);
+        recordsButton.onClick.AddListener(OpenRecordsView);
+        creditsButton.onClick.AddListener(OpenCreditsView);
+        recordsBackButton.onClick.AddListener(BackToMenuFromRecords);
+        creditsBackButton.onClick.AddListener(BackToMenuFromCredits);
         quitGame.onClick.AddListener(QuitGame);
     }
     public void OpenNewGame()
     {
         Debug.Log("OpenNewGame");
         SceneManager.LoadScene(1);
+    }
+
+    public void OpenRecordsView()
+    {
+        mainMenu.gameObject.SetActive(false);
+        recordsView.gameObject.SetActive(true);
+    }
+
+    public void OpenCreditsView()
+    {
+        mainMenu.gameObject.SetActive(false);
+        creditsView.gameObject.SetActive(true);
+    }
+
+    public void BackToMenuFromRecords()
+    {
+        mainMenu.gameObject.SetActive(true);
+        recordsView.gameObject.SetActive(false);
+    }
+
+    public void BackToMenuFromCredits()
+    {
+        mainMenu.gameObject.SetActive(true);
+        creditsView.gameObject.SetActive(false);
     }
 
     public void QuitGame()
